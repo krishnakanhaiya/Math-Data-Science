d58bdfb1f334e8a4f872addda4932419ee1d41f7
Krishna Kanhaiya <kanhaiya208@gmail.com>
2023-01-13 13:09:27 +0530

    Update Firstpro.r

--- Firstpro.r
+++ Firstpro.r
@@ -3,8 +3,11 @@ data=read.csv("c:\\Users\\Jai Maa Lakshmi Maa\\Videos\\Pythonp\\R\\mat.csv")
   
 # Create a matrix
 matrixdata=as.matrix(data)
+print(matrixdata)
+
+# Read data from CSV
+data1=read.csv("c:\\Users\\Jai Maa Lakshmi Maa\\Videos\\Pythonp\\R\\mathv.csv")
   
 # Create a vector
-vectordata=as.vector(data)
-print(matrixdata)
+vectordata=as.vector(data1)
 print(vectordata)
