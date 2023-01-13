4854439ec08818a9329cd8fee59574908b5b24e8
Krishna Kanhaiya <kanhaiya208@gmail.com>
2023-01-13 09:12:47 +0530

    Add files via upload

---
+++ Firstpro.r
@@ -0,0 +1,10 @@
+# Read data from CSV
+data=read.csv("c:\\Users\\Jai Maa Lakshmi Maa\\Videos\\Pythonp\\R\\mat.csv")
+  
+# Create a matrix
+matrixdata=as.matrix(data)
+  
+# Create a vector
+vectordata=as.vector(data)
+print(matrixdata)
+print(vectordata)
