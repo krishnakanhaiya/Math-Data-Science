5263ad0d25b99ece885288550967ae0bdabe3d85
Krishna Kanhaiya <kanhaiya208@gmail.com>
2023-01-14 22:13:21 -0800

    Add files via upload

---
+++ main1.py
@@ -0,0 +1,21 @@
+import pandas as pandas
+import numpy as numpy
+
+matrixA = numpy.array(pandas.read_csv("MatrixA.csv", sep =',', header = None, index_col = False))#.astype("float")
+numberOfRows_MatrixA, numberOfColumns_MatrixA = matrixA.shape
+vectorB = numpy.array(pandas.read_csv("VectorB.csv", sep =',', header = None, index_col = False))#.astype("float")
+numberOfRows_VectorB, numberOfColumns_VectorB = vectorB.shape
+print("Matrix A\t\t\tVector B")
+for i in range(numberOfRows_MatrixA):
+    for j in range(numberOfColumns_MatrixA):
+        space = ' '
+        if(matrixA[i][j] < 0):
+            space = ''
+        print(space + format(numpy.round(matrixA[i][j], 2)), end = "\t")
+    print(" ", end = "\t")
+    for j in range(numberOfColumns_VectorB):
+        space = ' '
+        if(vectorB[i][j] < 0):
+            space = ''
+        print(space + format(numpy.round(vectorB[i][j], 2)), end = "\t")
+    print(" ")
